831199bcdfeac62bfcd78cb02da38023214ce90d
palastiimre <palastiimre@gmail.com>
2021-06-09 21:21:03 +0200

    mod registration.component.html mod registration.component.ts

--- frontend/src/app/registration/registration.component.ts
+++ frontend/src/app/registration/registration.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import {environment} from "../../environments/environment";
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
+import {EmployeeService} from "../services/employee.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-registration',
@@ -8,12 +10,39 @@ import {environment} from "../../environments/environment";
 })
 export class RegistrationComponent implements OnInit {
   hide = true;
-  private BASE_URL = environment.apiUrl+'api/registration';
 
 
-  constructor() { }
+  registrationForm = this.formBuilder.group({
+    firstName: [null, [Validators.required]],
+    lastName: [null, [Validators.required]],
+    email: [null, [Validators.required]],
+    role: [null, [Validators.required]],
+    position: [null, [Validators.required]],
+    password: [null, [Validators.required]],
+    confirmPassword: [null, [Validators.required]],
+  });
+
+
+  constructor(private formBuilder: FormBuilder, private employeeService: EmployeeService,private router: Router) {
+  }
 
   ngOnInit(): void {
   }
 
+  submitRegistration() {
+    let employeeFormModel = this.registrationForm.value;
+    this.employeeService.registerEmployee(employeeFormModel)
+      .subscribe(response => {
+          console.log(response);
+        },
+        error => {
+          console.warn(error);
+
+        },
+        () => {
+          this.router.navigate(['/login'])
+        },
+      );
+    console.log(this.registrationForm.value)
+  }
 }
