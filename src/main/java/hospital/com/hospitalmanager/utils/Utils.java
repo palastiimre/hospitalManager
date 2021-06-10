34b8bac5b67c5ef595a1ef2ad46ef0cc7dfb7c35
palastiimre <palastiimre@gmail.com>
2021-06-10 15:36:14 +0200

    mod Utils

--- src/main/java/hospital/com/hospitalmanager/utils/Utils.java
+++ src/main/java/hospital/com/hospitalmanager/utils/Utils.java
@@ -11,12 +11,12 @@ public class Utils {
     private final Random RANDOM = new SecureRandom();
     private final String ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    public String generateUserId(int length){
+    public String generateUserId(int length) {
         return generateRandomString(length);
     }
 
     private String generateRandomString(int length) {
-        StringBuilder returnValue =new StringBuilder(length);
+        StringBuilder returnValue = new StringBuilder(length);
 
         for (int i = 0; i < length; i++) {
             returnValue.append(ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length())));
